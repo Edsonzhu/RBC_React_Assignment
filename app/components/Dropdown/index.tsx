dc2c1ae1daaf3eef43d2273243328437a8a15ef6
Edson <edsonfkz@gmail.com>
2025-11-16 23:44:36 -0500

    Adding dropdown component

---
+++ app/components/Dropdown/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+
+// config
+import { DropdownDefaultCss } from "@/config/components/dropdown"
+
+// assets
+import ArrowDownIcon from "@/assets/ArrowDownIcon";
+
+interface DropdownProps {
+  options: { value: string | number }[];
+  label?: string;
+  onClick: (value: string | number) => void;
+  component?: string;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ label, options, onClick, component }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [Component, setComponent] = useState<React.ComponentType<any> | null>()
+
+  useEffect(() => {
+    if (component)
+    {
+    import(`../${component}/index.tsx`)
+      .then(module => setComponent(() => module.default))
+      // If there is a failure on dynamically importing the component, just log the error
+      .catch((e) => console.log(`Error loading custom component: ${component} - ${e.message}`));
+    }
+  }, [])
+
+  const handleToggleMenu = () => {
+    setIsOpen((status) => (!status))
+  }
+
+  const handleOptionClick = (value: string | number) => {
+    handleToggleMenu();
+    onClick(value);
+  }
+
+  return (
+    <div className={DropdownDefaultCss.wraper} >
+      <button onClick={handleToggleMenu} className={DropdownDefaultCss.button} >
+        {label && <span>{label}</span>}
+        <ArrowDownIcon />
+      </button>
+      {
+        isOpen &&
+        <div className={DropdownDefaultCss.menu}>
+          {
+            options.map(({ value }, index) => (
+              Component
+              ? <div key={index} 
+                className={DropdownDefaultCss.option}
+                onClick={() =>  handleOptionClick(value)}>
+                <Component value={value} />
+              </div>
+              : <a
+                key={index}
+                href="#"
+                onClick={() => handleOptionClick(value)}
+                className={DropdownDefaultCss.option}
+              >{value}</a>
+            ))
+          }
+        </div>
+      }
+  </div>
+  )
+}
+
+export default Dropdown
